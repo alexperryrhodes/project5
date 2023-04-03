164b1943c5f3909b0ff0eb03842c2797e901ab28
Alexandra Rhodes <atperry30@gmail.com>
2023-04-03 15:32:10 -0700

    Changed routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./routes/Layout";
-import DetailView from './routes/DetailView';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
-    {/* <Routes>
-      <Route path="/" element={<Layout />}>
-                <Route index={true} element={<App />} />
-                <Route index={false} path="/WeatherDetail/:hour" element={<DetailView />} />
-      </Route>
-    </Routes> */}
   </BrowserRouter>
 );
