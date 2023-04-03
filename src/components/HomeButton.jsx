a429b66e505c9f1b0881e617264bb0625d9e006d
Alexandra Rhodes <atperry30@gmail.com>
2023-04-03 15:29:57 -0700

    Added home button

---
+++ src/components/HomeButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeButton = () => {
+  return (
+    <div className='home-link'>
+            <a className="menu-link" href="/">
+                Home
+            </a>
+    </div>
+  );
+}
+
+export default HomeButton;
