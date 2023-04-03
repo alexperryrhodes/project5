6557951f93be1c59830ec669bf03fb101fa1de80
Alexandra Rhodes <atperry30@gmail.com>
2023-04-03 13:55:56 -0700

    Updated WeatherStat

--- src/components/WeatherStat.jsx
+++ src/components/WeatherStat.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
-function WeatherStat({ weatherArray, stat }) {
-  const arraySize = weatherArray && weatherArray.length;
+function WeatherStat({ weatherJSON, stat }) {
+  //console.log("weatherArray", weatherJSON);
+  //console.log("stat", stat)
+
+  const arraySize = weatherJSON && weatherJSON.length;
+  //console.log("arraySize", arraySize)
+
   let sum = 0;
-    for (let i = 0; i < 24; i++) {
-    sum = sum + weatherArray[i];
+  for (let i = 0; i < arraySize; i++) {
+    const weatherItem = weatherJSON[i];
+    const statValue = weatherItem[stat];
+    //console.log(statValue);
+    sum = sum + statValue;
   }
   const avg = Math.round((sum / arraySize) * 10) / 10;
 
-    return (
-        < div className="stat" >
-            <p>{ stat }</p>
-    { avg }</div >);
+  return (
+    <div className="stat">
+      <div className="statName">
+      <p>Average</p>
+      <p>{stat}</p>
+      </div>
+      <p>{avg}</p>
+    </div>
+  );
 }
 
 export default WeatherStat;
